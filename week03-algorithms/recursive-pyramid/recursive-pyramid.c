138a94456871ce85a05e1b7879bb74e2019904ce
Mário Hermes <mariochermes@outlook.com>
2022-11-11 12:24:39 -0300

    Created recursive-pyramid for pratice

---
+++ week03-algorithms/recursive-pyramid/recursive-pyramid.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void draw(int height);
+
+int total_height;
+int aux;
+
+int main(int argc, char *argv[])
+{
+    int height = atoi(argv[1]);
+    total_height = height;
+
+    draw(height);
+}
+
+// Even Pyramid
+void draw (int height)
+{ 
+    if (height == 0)
+    {
+        return;
+    }
+
+    draw(height - 1);
+
+    int spaces = total_height - height;
+    for (int i = 1; i <= spaces; i++)
+    {
+        printf(" ");
+    }
+    for (int i = 1; i <= height; i++)
+    {
+        printf("#");
+    }
+    for (int i = 0; i <= height; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
+
+// Odd pyramid
+void draw (int height)
+{ 
+    if (height == 0)
+    {
+        return;
+    }
+
+    draw(height - 1);
+
+    int spaces = total_height - height;
+    for (int i = 1; i <= spaces; i++)
+    {
+        printf(" ");
+    }
+    for (int i = 1; i <= height + aux; i++)
+    {
+        printf("#");
+    }
+    aux++;
+    printf("\n");
+}
