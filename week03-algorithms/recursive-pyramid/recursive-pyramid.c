ba3671aa5a7027fd0a597ee57220850b1aab7367
Mário Hermes <mariochermes@outlook.com>
2022-11-11 12:50:44 -0300

    Update recursive-pyramid.c

--- week03-algorithms/recursive-pyramid/recursive-pyramid.c
+++ week03-algorithms/recursive-pyramid/recursive-pyramid.c
@@ -16,30 +16,30 @@ int main(int argc, char *argv[])
 
 // Comment one of the below functions
 // Even Pyramid 
-void draw (int height)
-{ 
-    if (height == 0)
-    {
-        return;
-    }
+// void draw (int height)
+// { 
+//     if (height == 0)
+//     {
+//         return;
+//     }
 
-    draw(height - 1);
+//     draw(height - 1);
 
-    int spaces = total_height - height;
-    for (int i = 1; i <= spaces; i++)
-    {
-        printf(" ");
-    }
-    for (int i = 1; i <= height; i++)
-    {
-        printf("#");
-    }
-    for (int i = 0; i <= height; i++)
-    {
-        printf("#");
-    }
-    printf("\n");
-}
+//     int spaces = total_height - height;
+//     for (int i = 1; i <= spaces; i++)
+//     {
+//         printf(" ");
+//     }
+//     for (int i = 1; i <= height; i++)
+//     {
+//         printf("#");
+//     }
+//     for (int i = 0; i <= height; i++)
+//     {
+//         printf("#");
+//     }
+//     printf("\n");
+// }
 
 // Odd pyramid
 void draw (int height)
