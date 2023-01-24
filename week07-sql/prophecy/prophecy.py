ed513a171e05c5553c83608fc95f8b64887fd288
Mário Hermes <mariochermes@outlook.com>
2023-01-24 18:32:16 -0300

    Update in prophecy

--- week07-sql/prophecy/prophecy.py
+++ week07-sql/prophecy/prophecy.py
@@ -1,18 +1,25 @@
 from csv import DictReader
-import cs50
+from cs50 import SQL
 
-db = cs50.SQL("sqlite:///roster.db")
+# Create database
+open("sqlite:///roster.db", "w").close()
+db = SQL("sqlite:///roster.db")
 
+# Open csv File
 with open("students.csv", "r") as file:
+
+    # Create DictReader
     reader = DictReader(file)
 
+    # Iterate over CSV file
     for row in reader:
 
+        # Store the values in variables
         student_name = row["student_name"].strip()
         house_name = row["house"].strip()
         house_head = row["head"].strip()
 
+        # Inserts the values into database
         student_id = db.execute("INSERT INTO students (student_name) VALUES(?)", student_name)
         db.execute("INSERT OR IGNORE INTO houses (house_name, house_head) VALUES(?, ?)", house_name, house_head)
-
         db.execute("INSERT INTO assignments (student_id, house_id) VALUES (?, (SELECT id from houses WHERE house_name = ?))", student_id, house_name)
