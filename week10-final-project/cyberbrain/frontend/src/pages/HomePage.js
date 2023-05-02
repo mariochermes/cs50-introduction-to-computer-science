c7592eac3f862b9c7d15081234652f6ebc8c7fe9
Mário Hermes <mariochermes@outlook.com>
2023-05-02 23:10:22 +0000

    May 2, 2023, 8:10 PM

---
+++ week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+
+
+function HomePage() {
+    return (
+      <div>
+        <h1>Welcome to my app!</h1>
+        <p>This is the home page.</p>
+      </div>
+    );
+  }
+  
+  export default HomePage;
