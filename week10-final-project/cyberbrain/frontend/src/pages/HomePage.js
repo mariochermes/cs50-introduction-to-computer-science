928a49ec2c0fa1881c83a9a6ee1114db890d1ae2
Mário Hermes <mariochermes@outlook.com>
2023-05-03 18:08:35 +0000

    May 3, 2023, 3:08 PM

--- week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
+++ week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
@@ -3,8 +3,8 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const HomePage = () => {
   return (
-    <Container className="w-75">
-      <Row className="text-center justify-content-center">
+    <Container className="w-75 h-100">
+      <Row className="h-100 justify-content-center align-items-center">
         <Col className="text-center">
           <h1>Welcome to Cyber Brain</h1>
           <p>A second brain to organize your life.</p>
