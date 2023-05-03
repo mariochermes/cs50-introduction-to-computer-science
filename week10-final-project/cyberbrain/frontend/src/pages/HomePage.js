0ea0a934709c4324cd2fe1171b69f932d4da0cad
Mário Hermes <mariochermes@outlook.com>
2023-05-03 11:44:43 +0000

    May 3, 2023, 8:44 AM

--- week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
+++ week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
@@ -1,14 +1,17 @@
-
+import React from 'react';
+import { Container, Row, Col } from '@mui';
 
 function HomePage() {
-    return (
-      <body>
-        <main>
-          <h1>Welcome to my app!</h1>
-          <p>This is the home page.</p>
-        </main>        
-      </body>
-    );
-  }
-  
-  export default HomePage;
+  return (
+    <Container>
+      <Row className="justify-content-center align-items-center" style={{ height: "100vh" }}>
+        <Col md={6} className="text-center">
+          <h1>Welcome to Cyber Brain</h1>
+          <p>Your one-stop solution for organizing your projects and notes</p>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default HomePage;
