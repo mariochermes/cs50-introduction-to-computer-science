165a3b5397dcdd0d212d9a20ac65f7096ab4d707
Mário Hermes <mariochermes@outlook.com>
2023-05-03 12:45:36 +0000

    May 3, 2023, 9:45 AM

--- week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
+++ week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
@@ -3,9 +3,9 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 const HomePage = () => {
   return (
-    <Container fluid>
-      <Row className="justify-content-center align-items-center vh-100">
-        <Col md={6} className="text-center">
+    <Container fluid className="d-flex align-items-center justify-content-center h-100">
+      <Row className="text-center">
+        <Col>
           <h1>Welcome to Cyber Brain</h1>
           <p>A second brain to organize your life.</p>
         </Col>
