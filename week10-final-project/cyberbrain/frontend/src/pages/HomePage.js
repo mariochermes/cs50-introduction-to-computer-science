7e8cdfc96395855a65e2b5c1b2c803adcab3ed23
Mário Hermes <mariochermes@outlook.com>
2023-05-04 19:26:31 +0000

    May 4, 2023, 4:26 PM

--- week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
+++ week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
@@ -1,41 +1,44 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Button, Container, Image } from 'react-bootstrap';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 import { publicUrl } from '../index';
 
 const HomePage = () => {
   return (
-    <Container className="d-flex flex-column align-items-center justify-content-center h-100" style={{ height: '100vh' }}>
-      <Image src={`${publicUrl}/sunIconDark.png`} className="align-self-end" width={25} height={25} />
-      <Image src={`${publicUrl}/cyberbrainLogo.png`} width={300} height={300}/>
-      <h1 className="text-center mb-5">Cyberbrain</h1>
-      <Image src={`${publicUrl}/cyberElement.png`} className="mb-5" />
-      <div className="d-flex justify-content-center w-100">
-        <Button variant="primary" className="m-2">
-          <Link to="/tasks" className="text-white text-decoration-none">
-            <i className="bi bi-card-checklist"></i>
-            <span className="ms-2">Tasks</span>
-          </Link>
-        </Button>
-        <Button variant="primary" className="m-2">
-          <Link to="/areas" className="text-white text-decoration-none">
-            <i className="bi bi-columns"></i>
-            <span className="ms-2">Areas</span>
-          </Link>
-        </Button>
-        <Button variant="primary" className="m-2">
-          <Link to="/resources" className="text-white text-decoration-none">
-            <i className="bi bi-folder2-open"></i>
-            <span className="ms-2">Resources</span>
-          </Link>
-        </Button>
-        <Button variant="primary" className="m-2">
-          <Link to="/archives" className="text-white text-decoration-none">
-            <i className="bi bi-archive"></i>
-            <span className="ms-2">Archives</span>
-          </Link>
-        </Button>
-      </div>
+    <Container className="d-flex flex-column align-items-center justify-content-center h-100 min-vh-100" maxWidth={1200}>
+      <Row className="justify-content-end w-100">
+        <Col xs="auto">
+        <Image src={`${publicUrl}/sunIconDark.png`} className="align-self-end" width={30} height={30} />
+        </Col>
+      </Row>
+      <Row className="align-items-center justify-content-center text-center w-100">
+        <Col xs="auto">
+          <Image fluid src={`${publicUrl}/cyberbrainLogo.png`} width={350} height={350} />
+          <h1 className="my-4">Cyberbrain</h1>
+          <Image src={`${publicUrl}/cyberElement.png`}/>
+        </Col>
+      </Row>
+      <Row className="w-100">
+        <Col xs="auto" className="text-center w-100">
+          <Row className="justify-content-evenly">
+            <Col xs="auto" className="text-center">
+              <img src={`${publicUrl}/cyberbrainLogo.png`} alt="Tasks Icon" width={64} height={64} />
+              <h5 className="my-3">Tasks</h5>
+            </Col>
+            <Col xs="auto" className="text-center">
+              <img src={`${publicUrl}/cyberbrainLogo.png`} alt="Areas Icon" width={64} height={64} />
+              <h5 className="my-3">Areas</h5>
+            </Col>
+            <Col xs="auto" className="text-center">
+              <img src={`${publicUrl}/cyberbrainLogo.png`} alt="Resources Icon" width={64} height={64} />
+              <h5 className="my-3">Resources</h5>
+            </Col>
+            <Col xs="auto" className="text-center">
+              <img src={`${publicUrl}/cyberbrainLogo.png`} alt="Archives Icon" width={64} height={64} />
+              <h5 className="my-3">Archives</h5>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 };
