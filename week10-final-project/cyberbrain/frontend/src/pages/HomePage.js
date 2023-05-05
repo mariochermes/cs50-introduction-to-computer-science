f7c07eb6a9b2a798003b38d7000480397c01d024
Mário Hermes <mariochermes@outlook.com>
2023-05-05 21:18:27 +0000

    May 5, 2023, 6:18 PM

--- week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
+++ week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
@@ -12,34 +12,34 @@ const HomePage = () => {
         </Col>
       </Row>
       <Row className="align-items-center justify-content-center text-center w-100">
-        <Col xs={8} sm={6} lg={4}>
-          <Image fluid src={`${publicUrl}/cyberbrain-logo-512.png`} className="mb-3" maxWidth={350} maxHeight={350} />
-          <h1 className="my-3">CyberBrain</h1>
+        <Col>
+          <Image src={`${publicUrl}/cyberbrain-logo-512.png`} width={350} style={{ maxWidth:'100%' }} />
+          <h1>CyberBrain</h1>
         </Col>
         <SteppedLineTo from="A" to="B" orientation="v" />
       </Row>
       <Row className="pb-5 w-100 h-100">
         <Col className="text-center w-100" xs={12} sm={8} lg={4}>
           <Row className="justify-content-evenly">
-            <Col xs={6} md={2} className="text-center mb-3">
+            <Col xs={6} md={3} lg={2} className="text-center mb-3">
               <button className="rounded-2 w-100 h-100">
                 <img src={`${publicUrl}/tasks-icon.png`} alt="Tasks Icon" className="mb-2" width={64} height={64} />
                 <h5>Tasks</h5>
               </button>
             </Col>
-            <Col xs={6} md={2} className="text-center mb-3">
+            <Col xs={6} md={3} lg={2} className="text-center mb-3">
               <button className="rounded-2 w-100 h-100">
                 <img src={`${publicUrl}/areas-icon.png`} alt="Areas Icon" className="mb-2" width={64} height={64} />
                 <h5>Areas</h5>
               </button>
             </Col>
-            <Col xs={6} md={2} className="text-center mb-3">
+            <Col xs={6} md={3} lg={2} className="text-center mb-3">
               <button className="rounded-2 w-100 h-100">
                 <img src={`${publicUrl}/resources-icon.png`} alt="Resources Icon" className="mb-2" width={64} height={64} />
                 <h5>Resources</h5>
               </button>
             </Col>
-            <Col xs={6} md={2} className="text-center mb-3">
+            <Col xs={6} md={3} lg={2} className="text-center mb-3">
               <button className="rounded-2 w-100 h-100">
                 <img src={`${publicUrl}/archives-icon.png`} alt="Archives Icon" width={64} height={64} />
                 <h5>Archives</h5>
