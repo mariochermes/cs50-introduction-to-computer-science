ca89aca2e4b07be7e61767ccb884b15b14fef687
Mário Hermes <mariochermes@outlook.com>
2023-05-08 14:03:20 +0000

    May 8, 2023, 11:03 AM

--- week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
+++ week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
@@ -19,29 +19,28 @@ const HomePage = () => {
       </Row>
       {/* <SteppedLineTo borderColor="#B7B7B7" from="page-title" to="tasks-page-button" orientation="v" fromAnchor="bottom" toAnchor="top" /> */}
       <Row className="w-100 h-100">
-        <Col className="text-center w-100" xs={12} sm={8} lg={4}>
+        <Col className="text-center w-100" xs={12} lg={4}>
           <Row className="justify-content-evenly">
             <Col xs={6} md={3} lg={2} className="text-center mb-3">
               <button className="tasks-page-button align-items-center d-flex flex-column h-100 justify-content-center pt-2 rounded-2 w-100" style={{ minHeight:'120px' }}>
                 <img src={`${publicUrl}/tasks-icon.png`} alt="Tasks Icon" width={60} height={60} />
                 <h5 className="mt-auto">Tasks</h5>
               </button>
-
             </Col>
             <Col xs={6} md={3} lg={2} className="text-center mb-3">
-              <button className="align-items-center d-flex flex-column h-100 justify-content-center pt-2 rounded-2 w-100">
+              <button className="tasks-page-button align-items-center d-flex flex-column h-100 justify-content-center pt-2 rounded-2 w-100" style={{ minHeight:'120px' }}>
                 <img src={`${publicUrl}/areas-icon.png`} alt="Areas Icon" width={60} height={60} />
                 <h5 className="mt-auto">Areas</h5>
               </button>
             </Col>
             <Col xs={6} md={3} lg={2} className="text-center mb-3">
-              <button className="align-items-center d-flex flex-column h-100 justify-content-center pt-2 rounded-2 w-100">
+              <button className="tasks-page-button align-items-center d-flex flex-column h-100 justify-content-center pt-2 rounded-2 w-100" style={{ minHeight:'120px' }}>
                 <img src={`${publicUrl}/resources-icon.png`} alt="Resources Icon" width={60} height={60} />
                 <h5 className="mt-auto">Resources</h5>
               </button>
             </Col>
             <Col xs={6} md={3} lg={2} className="text-center mb-3">
-              <button className="align-items-center d-flex flex-column h-100 justify-content-center pt-2 rounded-2 w-100">
+              <button className="tasks-page-button align-items-center d-flex flex-column h-100 justify-content-center pt-2 rounded-2 w-100" style={{ minHeight:'120px' }}>
                 <img src={`${publicUrl}/archives-icon.png`} alt="Archives Icon" width={60} height={60} />
                 <h5 className="mt-auto">Archives</h5>
               </button>
