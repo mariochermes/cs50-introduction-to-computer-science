c06df9453ea84a0e107ba2e18582d4cf369ca3d5
Mário Hermes <mariochermes@outlook.com>
2023-05-11 23:40:42 +0000

    May 11, 2023, 8:40 PM

--- week10-final-project/cyberbrain/frontend/src/components/PageButton.js
+++ week10-final-project/cyberbrain/frontend/src/components/PageButton.js
@@ -4,7 +4,7 @@ import { publicUrl } from '../index';
 export default function PageButton({ pageName, iconUrl }) {
     return (
     <Button className="tasks-page-button align-items-center d-flex flex-column h-100 justify-content-center pt-2 rounded-2 w-100" style={{ minHeight:'120px' }}>
-        <Image src={`${publicUrl}/${pageName}.png`} alt={pageName} width={60} height={60} />
+        <Image src={`${publicUrl}/${pageName}.png`} alt={`{pageName} icon`} width={60} height={60} />
         <h5 className="mt-auto">Archives</h5>
       </Button>
     );
