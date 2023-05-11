55243fc51ec6d9feaab4efcfeea40cfe862cf28b
Mário Hermes <mariochermes@outlook.com>
2023-05-11 23:12:36 +0000

    May 11, 2023, 8:12 PM

--- week10-final-project/cyberbrain/frontend/src/components/PageButton.js
+++ week10-final-project/cyberbrain/frontend/src/components/PageButton.js
@@ -1,10 +1,10 @@
 import { Button, Container, Image, Nav, Navbar, Offcanvas } from 'react-bootstrap';
 import { publicUrl } from '../index';
 
-export default function PageButton() {
+export default function PageButton({ pageName, iconUrl }) {
     return (
     <Button className="tasks-page-button align-items-center d-flex flex-column h-100 justify-content-center pt-2 rounded-2 w-100" style={{ minHeight:'120px' }}>
-        <Image src={`${publicUrl}/archives-icon.png`} alt="Archives Icon" width={60} height={60} />
+        <Image src={`${publicUrl}/${pageName}.png`} alt={pageName} width={60} height={60} />
         <h5 className="mt-auto">Archives</h5>
       </Button>
     );
