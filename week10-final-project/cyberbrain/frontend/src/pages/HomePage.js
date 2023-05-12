5fe3b514708ba88ac9c4630884cc169b133e834d
Mário Hermes <mariochermes@outlook.com>
2023-05-12 17:51:55 +0000

    May 12, 2023, 2:51 PM

--- week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
+++ week10-final-project/cyberbrain/frontend/src/pages/HomePage.js
@@ -7,7 +7,9 @@ import { SteppedLineTo } from 'react-lineto';
 
 export default function HomePage() {
   return (
-    <Container className="align-items-center d-flex flex-column flex-grow-1 justify-content-evenly h-100 py-5">
+    <Container className="align-items-center d-flex flex-column flex-grow-1 justify-content-between h-100 py-5">
+      <Row className="w-100">
+      </Row>
       <Row className="align-items-center justify-content-center text-center w-100">
         <Col xs="auto">
           <Image alt="CyberBrain logo" src={`${publicUrl}/cyberbrain-logo-512.png`} width={300}/>
