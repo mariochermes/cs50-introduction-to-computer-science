e8844f3b59d3a427b98fd5ad136f8ae2c1a1dac4
Mário Hermes <mariochermes@outlook.com>
2023-05-12 22:34:40 +0000

    May 12, 2023, 7:34 PM

--- week10-final-project/cyberbrain/frontend/src/components/PageButton.js
+++ week10-final-project/cyberbrain/frontend/src/components/PageButton.js
@@ -6,7 +6,7 @@ import '../styles/page-button.css';
 export default function PageButton({ pageName }) {
     return (
     <Button className="tasks-page-button align-items-center d-flex flex-column h-100 justify-content-center p-0 pt-2 w-100">
-        <Image alt={`${pageName} icon`} src={`${publicUrl}/${pageName}-icon.png`} width={60}/>
+        <Image alt={`${pageName} icon`} className="page-icon" src={`${publicUrl}/${pageName}-icon.png`} width={60}/>
         <h5 className="w-100">{pageName.charAt(0).toUpperCase() + pageName.slice(1)}</h5>
       </Button>
     );
